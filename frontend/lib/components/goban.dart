7af21a72b2cd6ff9d35e7be36600df3054c6150c
usatyo <yu0424.math8128@gmail.com>
2025-04-18 16:59:36 +0900

    add operation to goban

--- frontend/lib/components/goban.dart
+++ frontend/lib/components/goban.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/components/goban_painter.dart';
+import 'package:frontend/components/icon_text.dart';
 import 'package:frontend/components/single_stone.dart';
 import 'package:frontend/models/joseki.dart';
 
@@ -13,59 +14,119 @@ class Goban extends StatefulWidget {
 class _GobanState extends State<Goban> {
   Joseki joseki = Joseki([]);
   StoneColor nextColor = StoneColor.black;
+  void reverseColor() {
+    setState(() {
+      nextColor =
+          (nextColor == StoneColor.black) ? StoneColor.white : StoneColor.black;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      width: 300,
-      child: InteractiveViewer(
-        panEnabled: false,
-        child: Stack(
-          children: [
-            CustomPaint(size: Size(300, 300), painter: GobanPainter()),
-            GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 13 * 13,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                if (joseki.stoneMap.containsKey(index)) {
-                  final stone = joseki.stoneMap[index]!;
-                  return SingleStone(
-                    num: joseki.stoneList.indexOf(stone) + 1,
-                    color: stone.color,
-                    onPressed: () {},
-                  );
-                }
-                return SingleStone(
-                  num: null,
-                  color: StoneColor.empty,
-                  onPressed:
-                      () => {
-                        setState(() {
-                          joseki.pushStone(
-                            Stone(
-                              color: nextColor,
-                              x: index ~/ 13,
-                              y: index % 13,
-                            ),
+    return IntrinsicHeight(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        spacing: 10,
+        children: [
+          Expanded(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: InteractiveViewer(
+                panEnabled: false,
+                child: Stack(
+                  children: [
+                    CustomPaint(size: Size.infinite, painter: GobanPainter()),
+                    GridView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: 13 * 13,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        if (joseki.stoneMap.containsKey(index)) {
+                          final stone = joseki.stoneMap[index]!;
+                          return SingleStone(
+                            num: joseki.stoneList.indexOf(stone) + 1,
+                            color: stone.color,
+                            onPressed: () {},
                           );
-                          nextColor =
-                              (nextColor == StoneColor.black)
-                                  ? StoneColor.white
-                                  : StoneColor.black;
-                        }),
+                        }
+                        return SingleStone(
+                          num: null,
+                          color: StoneColor.empty,
+                          onPressed:
+                              () => {
+                                setState(() {
+                                  joseki.pushStone(
+                                    Stone(
+                                      color: nextColor,
+                                      x: index ~/ 13,
+                                      y: index % 13,
+                                    ),
+                                  );
+                                  reverseColor();
+                                }),
+                              },
+                        );
                       },
-                );
-              },
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 13,
-                mainAxisSpacing: 2,
-                crossAxisSpacing: 2,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 13,
+                            mainAxisSpacing: 2,
+                            crossAxisSpacing: 2,
+                          ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            width: 40,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconText(
+                  hintText: "1手戻る",
+                  icon: Icons.keyboard_arrow_up,
+                  onPressed: () {
+                    setState(() {
+                      joseki.popStone();
+                      reverseColor();
+                    });
+                  },
+                ),
+                IconText(
+                  hintText: "5手戻る",
+                  icon: Icons.keyboard_double_arrow_up,
+                  onPressed: () {
+                    setState(() {
+                      joseki.popStones(5);
+                      reverseColor();
+                    });
+                  },
+                ),
+                IconText(
+                  hintText: "手抜き",
+                  icon: Icons.repeat,
+                  onPressed: () {
+                    setState(() {
+                      reverseColor();
+                    });
+                  },
+                ),
+                IconText(
+                  hintText: "クリア",
+                  icon: Icons.delete,
+                  onPressed: () {
+                    setState(() {
+                      joseki.clear();
+                      nextColor = StoneColor.black;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ frontend/lib/components/icon_text.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class IconText extends StatelessWidget {
+  const IconText({
+    super.key,
+    required this.hintText,
+    required this.icon,
+    required this.onPressed,
+  });
+  final String hintText;
+  final IconData icon;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton.filled(
+          onPressed: onPressed,
+          icon: Icon(icon, size: 20),
+          tooltip: hintText,
+        ),
+      ],
+    );
+  }
+}
