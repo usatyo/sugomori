612ce003609a249b48a7ffa20456349459b10fa0
usatyo <yu0424.math8128@gmail.com>
2025-05-27 19:18:35 +0900

    add 200 api test

--- backend/controller/joseki_test.go
+++ backend/controller/joseki_test.go
@@ -16,7 +16,7 @@ import (
 )
 
 var (
-	JosekiJSON = `{
+	RequestPostJosekiJSON = `{
 		"joseki": {"stones": [
 			{"x": 3, "y": 2, "color": 0},
 			{"x": 3, "y": 4, "color": 1},
@@ -28,13 +28,13 @@ var (
 	ResponsePostJosekiJSON = `{"message":"Joseki added"}` + "\n"
 )
 
-func TestPostJoseki(t *testing.T) {
+func TestPostJoseki200(t *testing.T) {
 	util.LoadEnvVar("../.env.dev")
 	db.Initialize()
 
 	e := echo.New()
 	e.Validator = &val.CustomValidator{Validator: validator.New()}
-	req := httptest.NewRequest(http.MethodPost, "/joseki", strings.NewReader(JosekiJSON))
+	req := httptest.NewRequest(http.MethodPost, "/joseki", strings.NewReader(RequestPostJosekiJSON))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
@@ -43,5 +43,35 @@ func TestPostJoseki(t *testing.T) {
 	if assert.NoError(t, controller.PostJosekiHandler(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, ResponsePostJosekiJSON, rec.Body.String())
+	}
+}
+
+var (
+	RequestGetVideoJSON = `{
+		"stones": [
+			{"x": 3, "y": 2, "color": 0},
+			{"x": 3, "y": 4, "color": 1},
+			{"x": 2, "y": 4, "color": 0},
+			{"x": 2, "y": 3, "color": 1}
+		]
+	}`
+	ResponseGetVideoJSON = `{"data":[{"id":"aj0I_Z6bK9E"}]}` + "\n"
+)
+
+func TestGetVideo200(t *testing.T) {
+	util.LoadEnvVar("../.env.dev")
+	db.Initialize()
+
+	e := echo.New()
+	e.Validator = &val.CustomValidator{Validator: validator.New()}
+	req := httptest.NewRequest(http.MethodPost, "/video", strings.NewReader(RequestGetVideoJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Assertions
+	if assert.NoError(t, controller.GetVideoHandler(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, ResponseGetVideoJSON, rec.Body.String())
 	}
 }
