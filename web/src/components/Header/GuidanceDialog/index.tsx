a55ead62b1ffc4ecdbc6b629b1a714412cb2ce46
usatyo <yu0424.math8128@gmail.com>
2025-12-30 03:19:30 +0900

    add inquiry information

--- web/src/components/Header/GuidanceDialog/index.tsx
+++ web/src/components/Header/GuidanceDialog/index.tsx
@@ -8,12 +8,18 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Separator } from "@/components/ui/separator"
-import { CircleQuestionMarkIcon } from "lucide-react"
+import { CircleQuestionMarkIcon, CopyIcon } from "lucide-react"
 import type { FC } from "react"
+import { toast } from "sonner"
 
 type Props = {}
 
 const GuidanceDialog: FC<Props> = () => {
+  const onClickEmail = () => {
+    navigator.clipboard.writeText("dtotb33333@gmail.com")
+    toast.info("メールアドレスをコピーしました")
+  }
+  
   return (
     <Dialog>
       <DialogTrigger className="cursor-pointer">
@@ -26,25 +32,37 @@ const GuidanceDialog: FC<Props> = () => {
           </DialogTitle>
         </DialogHeader>
         <Separator />
-        <h2 className="font-bold">碁盤で検索</h2>
+        <h2 className="font-bold">碁盤・URLで検索</h2>
         <p className="px-2">
           定石手順を入力することで、登録済みの動画から似た内容のものを検索できます。回転・反転した盤面で検索しても問題ありません。
-        </p>
-        <Separator />
-        <h2 className="font-bold">URLで検索</h2>
-        <p className="px-2">
-          URLを貼り付けることで、動画を検索できます。検索結果から対象の動画をクリックして、下の碁盤から動画と関連する定石手順を入力すれば、「碁盤で検索」から検索できるようになります。関連する定石は最大で5個まで追加可能です。
+          「動画に関連する定石手順」を変更することで、碁盤検索の結果に影響を与えることができます。
         </p>
         <Separator />
         <h2 className="font-bold">注意事項</h2>
         <p className="px-2">
           sugomori
-          はユーザーが投稿した動画情報と定石手順を元に動作しています。動画や定石手順の内容に関する責任は負いかねますのでご了承ください。
+          はユーザーが入力した定石手順を元に動作しています。動画や定石手順の内容に関する責任は負いかねますのでご了承ください。
+          また、入力された定石手順はサービス向上のために利用される場合があります。
         </p>
-        {/* <Separator />
+        <Separator />
         <p className="px-2">
-          ご意見・ご要望がありましたら、までご連絡ください。
-        </p> */}
+          {/* 詳細は
+          <a href="" target="_blank" rel="noopener noreferrer">
+            こちら
+          </a>
+          の動画から確認いただけます。
+          <br /> */}
+          お問い合わせがありましたら、
+          <Button
+            variant="link"
+            className="cursor-pointer text-md"
+            onClick={onClickEmail}
+          >
+            dtotb33333@gmail.com
+            <CopyIcon className="inline mt-1" size={16} />
+          </Button>
+          までご連絡ください。
+        </p>
         <DialogClose>
           <Button className="w-1/2">OK</Button>
         </DialogClose>
